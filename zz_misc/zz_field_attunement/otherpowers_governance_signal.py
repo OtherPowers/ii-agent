1ae02f2b50976ee592989c92c9145592a1181085
OtherPowers <44530314+OtherPowers@users.noreply.github.com>
2026-01-03 12:38:44 -0800

    Remove zz_misc experimental folder

--- zz_misc/zz_field_attunement/otherpowers_governance_signal.py
+++
@@ -1,7 +0,0 @@
-from otherpowers_governance.signals.schema import (
-    OtherPowers_GovernanceSignalRecord,
-    new_signal,
-)
-
-OtherPowers_GovernanceSignal = OtherPowers_GovernanceSignalRecord
-
